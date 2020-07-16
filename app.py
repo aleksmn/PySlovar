cc21c39b5e3a6fa2008de48a1e110827c90113d8
Mikhail Aleksandrov <al-mn@yandex.ru>
2020-07-16 14:08:28 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+
+file = open('words.xml')
+
+soup = BeautifulSoup(file, 'html.parser')
+
+words = []
+
+for t in soup.find_all('title'):
+    title = t.text.strip()
+    word = title.split(' ')[0]
+    if len(word) < 2: continue
+    words.append(word)
+
+def search(query):
+    for s in soup.select('section > section'):
+        title = s.find('title').text.strip()
+        if query.title() in title:
+            for p in s.select('p'):
+                    print(p.text)
+            print()
+        else:
+            continue
+
+def start_search():
+    while True:
+        query = input('Введите слово для поиска: ')
+        if query == 'q':
+            print('До свидания!')
+            break
+        search(query)
+
+try:
+    print('Добро пожаловать в Словарь!')
+    q = input('Начать поиск (1) или смотреть слова (2): ')
+
+    if q == '1':
+        start_search()
+    elif q == '2':
+        print('Список слов')
+        print(' '.join(words))
+        start_search()
+    else: print('Неверный ввод')
+except KeyboardInterrupt:
+    print('Прервано пользователем')
